6ee15cb0081ac8081cfff8f676ef9de1d2f21ecd
laptot <88231085+HasSak-47@users.noreply.github.com>
2021-10-07 21:08:40 -0500

    updated format to cmake
    Added A CMakeList.txt and build

---
+++ build/CMakeFiles/trees.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trees.dir/src/lib/image.cpp.o"
+  "CMakeFiles/trees.dir/src/main.cpp.o"
+  "trees"
+  "trees.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trees.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
