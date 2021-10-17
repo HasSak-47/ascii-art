94ee4d4eba5ca63c65dab4635791dbe67ba0911d
Daniel <ulises.alanis.255@gmail.com>
2021-10-17 10:09:49 -0500

    RELEASE

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,91 @@
 #include<exception>
 #include<iostream>
 #include<fstream>
-#include"headers/image.h"
+#include<memory>
+#include<cmath>
 
-using namespace sk;
+#define STB_IMAGE_IMPLEMENTATION
+#include<stb_image.h>
 
-int assign_color(char* str, color col){
-    char* c_format = color_text(col);
-    for(size_t i = 0; i < 9; ++i) str[i] = c_format[i];
-    return 9;
-}
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include<stb_image_resize.h>
 
-int get_color(byte* pixel){
-    return (
-        (*(pixel + 0) > 127) * 1 +
-        (*(pixel + 1) > 127) * 2 +
-        (*(pixel + 2) > 127) * 4 +
-        (*(pixel + 3) > 127) * 8
-    );
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include<stb_image_write.h>
+
+#define default_height 25
+#define default_width 50
+
+const char default_path[] = "ascii_art.txt";
+const char intensity[] = {' ', '#'};
+
+int str_to_int(const char* str){
+    int result = 0;
+    while(*str && *str >= '0' && *str <= '9'){
+        result *= 10;
+        result += *str - '0';
+        str++;
+    }
+    return result;
 }
 
 int main(int argc, char* argv[]){
-    if(argc == 1){
-        std::cerr << "no image provided\n";
-        return 1;
+    const char* path = default_path;
+    int output_height = default_height, possible_height;
+    int output_width  = default_width, possible_width;
+    switch (argc){
+    case 1:
+        std::cout << "not image path given\n";
+        return -1;
+    case 3:
+        path = argv[2];
+        break;
+    case 4:
+        possible_width = str_to_int(argv[3]);
+        if(possible_width == 0) break;
+        output_width = possible_width;
+        output_height = output_width / 2;
+        break;
+    case 5:
+        possible_width = str_to_int(argv[3]);
+        possible_height = str_to_int(argv[4]);
+        if(possible_width == 0|| possible_height == 0) break;
+        output_width = possible_width;
+        output_height = possible_height;
+        break;
+    default:
+        break;
     }
 
-    try{
-        image img(argv[1]);
+    int width, height, channels;
+    std::unique_ptr<unsigned char[]> image( stbi_load(argv[1], &width, &height, &channels, 0) );
 
-        if(img.channels != 4) throw std::runtime_error("not a transpartent img!");
+    if(!image.get()){
+        std::cout << "image at "<< argv[1] <<" could not be loaded\n";
+        return 1;
+    }
+    if(channels < 4){
+        std::cout << "image is not a png!\n";
+        return 1;
+    }
+    size_t new_img = (4 * (output_height) * output_width);
+    std::unique_ptr<unsigned char[]> resized = std::make_unique<unsigned char[]>(new_img);
+    stbir_resize_uint8(
+        image.get(), width, height, 0,
+        resized.get(), output_width, output_height, 0,
+        4
+    );
+    image.reset();
+    std::ofstream ascii_art(path);
 
-        //float ratio =(float)max_height / img.width;
-        //img.resize(ratio);
-        img.resize(2 * max_height, max_height);
-        img.save("scaled.png");
-
-        char* txt_data = new char[img.width * img.height * 11];
-        int txt_index = 0;
-        const int max_img_size = img.channels * img.height * img.width;
-        color prev_color = {};
-        for(size_t i = 0; i <  img.height; ++i){
-            for(size_t j = 0; j <  img.width; ++j){
-                int index = 4 * (i * img.width + j);
-                int colors = get_color(img.data + index);
-                if(colors <= 7 || (colors - 8) == 0) txt_data[txt_index++] = ' ';
-                else{
-                    colors -= 8;
-                    if((color)colors != prev_color){
-                        std::cout << colors << '\n';
-                        assign_color(&txt_data[txt_index], (color)colors);
-                        txt_index += 9;
-                        prev_color = (color)colors;
-                    }
-                    txt_data[txt_index++] = '#';
-                }
-            }
-            txt_data[txt_index++] = ' ';
-            txt_data[txt_index++] = '\n';
+    for(size_t i = 0; i < new_img / 4; ++i){
+        char intense = (resized[(4 * i) + 3] > 128)? '#' : ' ';
+        std::cout << intense;
+        ascii_art << intense;
+        if(i % output_width == (output_width - 1)) {
+            std::cout << '\n';
+            ascii_art << '\n';
         }
-
-        std::ofstream file("text.txt");
-        if(!file.is_open()) throw std::runtime_error("file not found!");
-        file.write(txt_data, txt_index);
-        file.close();
-        delete[] txt_data;
-        
-    }catch(std::runtime_error& err){
-        std::cout<< err.what() << '\n';
-        return -1;
     }
     return 0;
 }
