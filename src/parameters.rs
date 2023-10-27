1914f1bbaef3d3c8bf83ef1c48bd0687c45efb3c
arch machine <lilith.alanis.255@gmail.com>
2023-10-26 18:48:42 -0600

    split into files

---
+++ src/parameters.rs
@@ -0,0 +1,19 @@
+#[allow(dead_code)]
+#[derive(Debug, Default, Clone)]
+pub struct RgbTransform{
+    r: (f32, f32, f32, f32),
+    b: (f32, f32, f32, f32),
+    g: (f32, f32, f32, f32),
+    a: (f32, f32, f32, f32),
+}
+
+#[derive(Debug, Clone, Default)]
+pub enum ColorRange{
+    #[default]
+    Undefined,
+    Rgb(u8),
+    Rgba(u8),
+    Luma(u8),
+    LumaAlpha(u8),
+    RgbTransform(RgbTransform),
+}
--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,5 @@
 use crate::ColorRange;
+use crate::Options;
 
 pub trait Parse where Self: Sized{
     fn parse(s: &str) -> Self;
@@ -44,3 +45,52 @@ impl Parse for ColorRange{
 }
 
 pub fn parse<P: Parse>(s: &str) -> P{ P::parse(s) }
+
+fn locate(start: Option<usize>, end: usize, mat: char, data: &Vec<char>) -> Option<(usize, usize)> {
+    let start = start?;
+
+    for i in start..end{
+        if mat == data[i]{
+            return Some((start, i));
+        }
+    }
+
+    Some((start, end))
+}
+
+pub fn parse_option<S : AsRef<str>>(arg: S) -> Options{
+    let arg : &str = arg.as_ref();
+
+    // sets default options
+    let mut opt = Options::default();
+    opt.in_path = arg.to_string();
+    opt.out_path = "out.".to_string() + arg;
+    opt.size =  (44, 0);
+
+    let opt_div= arg.find(':');
+    // No options passed
+    if opt_div.is_none(){
+        return opt
+    }
+    let s_chars : Vec<char> = arg.chars().collect();
+    let s_chars_len = s_chars.len();
+
+    let in_path     = locate(Some(0), s_chars_len, ':', &s_chars);
+    let out_path    = locate(arg.find("out=")  , s_chars_len, ',', &s_chars);
+    let size        = locate(arg.find("size=") , s_chars_len, ',', &s_chars);
+    let color_range = locate(arg.find("color="), s_chars_len, ',', &s_chars);
+
+    if let Some(in_path) = in_path
+    { opt.in_path = parse(&arg[in_path.0..in_path.1]) }
+
+    if let Some(out_path) = out_path
+    { opt.out_path = parse(&arg[out_path.0 + 4..out_path.1]) }
+
+    if let Some(size) = size
+    { opt.size = parse(&arg[size.0 + 5..size.1]) }
+
+    if let Some(color_range) = color_range
+    { opt.color_range = parse(&arg[color_range.0 + 6..color_range.1]) }
+
+    opt
+}
