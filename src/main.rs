ea16e9dd0f1afc9589b59f28451242c3fb349a89
Ulises Alanis <ulises.alanis.255@gmail.com>
2023-10-26 18:32:56 -0600

    something something

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod edges;
 
 use image::{self, GenericImageView};
 use std::io::prelude::Write;
-use std::env;
+use std::{env, default};
 
 mod parser;
 use crate::parser::parse;
@@ -16,17 +16,15 @@ pub struct RgbTransform{
     a: (f32, f32, f32, f32),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub enum ColorRange{
+    #[default]
+    Undefined,
     Rgb(u8),
     Rgba(u8),
     Luma(u8),
     LumaAlpha(u8),
     RgbTransform(RgbTransform),
-}
-
-impl Default for ColorRange{
-    fn default() -> Self { ColorRange::Luma(8) }
 }
 
 #[derive(Default, Debug, Clone)]
@@ -91,6 +89,10 @@ fn write_rgb_color(p: u32, c: (u8, u8, u8)) -> String{
     format!("\x1b[48;2;{};{};{}m", c.0, c.1, c.2)
 }
 
+fn write_rgba_color(p: u32, c: (u8, u8, u8)) -> String{
+    format!("\x1b[38;2;{};{};{}m", c.0, c.1, c.2)
+}
+
 fn process_rgb(p: u32, img: image::RgbImage) -> String{
     let p = p as u8;
     let mut r = String::new();
@@ -113,11 +115,40 @@ fn process_rgb(p: u32, img: image::RgbImage) -> String{
     r
 }
 
+fn process_rgba(p: u32, img: image::RgbaImage) -> String{
+    let p = p as u8;
+    let mut r = String::new();
+    let mut latest_color = (0, 0, 0);
+    for row in img.rows(){
+        for pixel in row{
+            let current_color = (pixel.0[0], pixel.0[1], pixel.0[2]);
 
-fn process_image(opts: Options) {
+            if current_color != latest_color{
+                latest_color = current_color;
+                r += &write_rgba_color(p as u32, current_color);
+            }
+            r += if pixel.0[3] != 0 {"#"} else { " " };
+        }
+        println!();
+        r += "\n";
+    }
+    latest_color = (0, 0, 0);
+    r += &write_rgb_color(p as u32, latest_color);
+
+    r
+}
+
+
+fn process_image(mut opts: Options) {
     let img = image::open(&opts.in_path) .expect("file not found!");
     let dims = img.dimensions();
     let ratio = dims.1 as f32 / dims.0 as f32;
+    opts.color_range = match img.color(){
+        image::ColorType::Rgb8 => ColorRange::Rgb(8),
+        image::ColorType::Rgba8 => ColorRange::Rgba(8),
+        _ => {ColorRange::LumaAlpha(0)},
+    };
+    println!("{opts:?}");
     
 
     let resized = if opts.size.1 == 0 {
@@ -129,7 +160,11 @@ fn process_image(opts: Options) {
         img.resize_exact(opts.size.0, opts.size.1, image::imageops::Nearest)
     };
 
-    let img_ascii = process_rgb(16, resized.into_rgb8());
+    let img_ascii = match opts.color_range{
+        ColorRange::Rgba(p) => process_rgba(p as u32, resized.into_rgba8()),
+        _ => process_rgb(16, resized.into_rgb8()),
+    };
+
     println!("{img_ascii}");
     let mut file = std::fs::File::create(opts.out_path).unwrap();
     file.write(img_ascii.as_bytes()).unwrap();
@@ -146,39 +181,4 @@ fn main() {
     args.remove(0);
     let options : Vec<Options> = args.iter().map(|x| parse_option(x)).collect();
     options.iter().for_each(|x| process_image(x.clone()));
-
-    /*
-    let img = match image::open(args[0].clone()){
-        Ok(k) => k,
-        Err(_) => {
-            println!("file {args:?} not found");
-            return;
-        }
-    };
-    let height = 22;
-    let width  = height * 2;
-    let image = img.resize_exact(width * 2, height * 3, image::imageops::Nearest).into_luma8();
-
-    // let mut char_map = [[' '; 100]; 100];
-    let mut file = std::fs::File::create(format!("{}.txt", args[0])).unwrap();
-    let mut write = "\n".to_string();
-    for j in 0..height{
-        for i in 0..width{
-            let mut char_id : usize = 0;
-            for jd in 0..3u32{
-                for id in 0..2u32{
-                    let val : usize = 1 << (jd + id * 3) as usize;
-                    let valid = image.get_pixel(i * 2 + id, j * 3 + jd).0[0] >= 1;
-                    char_id |= val * valid as usize;
-                }
-            }
-            // print!(" {:02x}", char_id);
-            write.push(edges::CHARACTERS[char_id]);
-        }
-        write.push('\n');
-    }
-
-    // println!("{}", write);
-    file.write(write.as_bytes()).unwrap();
-    */
 }
